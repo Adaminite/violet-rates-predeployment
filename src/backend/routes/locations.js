d7d6047ea7a554c7020d716d73fb6e328ead86f5
Omar <niazi.omark@gmail.com>
2022-06-24 14:17:00 -0400

    Add post request to add location to DB

--- src/backend/routes/locations.js
+++ src/backend/routes/locations.js
@@ -15,8 +15,33 @@ router.get('/', (req, res) => {
     });
 });
 
-
-
+router.post('/add', (req, res) => {
+    let locations;
+    Locations.exists({
+        name: req.name.toLowerCase(),
+        address: req.address.toLowerCase()
+    }, (error, data) => {
+        if(error){
+            console.log("Error: Unable to add location");
+        }
+        else if(data != null){
+            console.log("Error: Already exists");
+        }
+        else{
+            Locations.create({
+                name: req.name.toLowerCase(),
+                address: req.address.toLowerCase()
+            }, (error, data) => {
+                if(error){
+                    res.send(error);
+                }
+                else{
+                    res.send("Successfully added");
+                }
+            });
+        }
+    });
+});
 
 
 module.exports = router;
