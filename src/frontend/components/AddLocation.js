193b8c0dbdee8d7fd3e60218e98e9107d46a9cff
Omar <niazi.omark@gmail.com>
2022-06-24 15:46:50 -0400

    Finish form and basic styling for it

--- src/frontend/components/AddLocation.js
+++ src/frontend/components/AddLocation.js
@@ -9,18 +9,48 @@ class AddLocation extends React.Component{
 
     render(){
         return(
-        <form>
-            <div className ="form-group">
-                <label for = "name"> Name of Location </label>
-                <input className="form-control" name = "name" type = "text" placeholder="Papa's Pizzeria"/>
-            </div>
-            
-            <div class = "form-group"> 
-                <label for = "address"> Address </label>
-                <input className="form-control" name = "address" placeholder = "12345 Cinderella Dr" type = "text"/>
-            </div>
+        <div>
+            <h1 style={{textAlign:"center", marginTop: "10px"}} class = "h1"> Add A Location </h1>
+            <div id = "add-form-ctnr">    
+                <form id = "add-form">
+                    <div className ="form-group">
+                        <label for = "name"> Name of Location </label>
+                        <input className="form-control" name = "name" type = "text" placeholder="Papa's Pizzeria" required/>
+                    </div>
+                    
+                    <div className = "form-group"> 
+                        <label for = "address"> Address Line 1 </label>
+                        <input className="form-control" name = "address" placeholder = "12345 Cinderella Dr" type = "text" required/>
+                    </div>
 
-        </form>
+                    <div className = "form-group"> 
+                        <label for = "address"> Address Line 2 </label>
+                        <input className="form-control" name = "address" placeholder = "Apt 106" type = "text" value = ""/>
+                    </div>
+
+                    <div className = "row">
+                        <div className = "form-group col-auto">
+                            <label for = "name"> City </label>
+                            <input className="form-control" name = "name" type = "text" placeholder="Los Angeles" required/>
+                        </div>
+
+                        <div className = "form-group  col-auto">
+                            <label for = "name"> State </label>
+                            <input className="form-control" name = "name" type = "text" placeholder="CA" required/>
+                        </div>
+
+                        <div className = "form-group  col-auto">
+                            <label for = "name"> Zip Code </label>
+                            <input className="form-control" name = "name" type = "text" placeholder="28742" required/>
+                        </div>
+
+                        <div id ="submit-btn-ctnr" className = "form-group">
+                            <input style={{backgroundColor: "#6f5499"}} class = "btn btn-primary" type = "submit" value = "Add Location"/> 
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
         );
     }
 }
