0ebae45d710c047c25b19c69f15d34bb99de67e0
Omar <niazi.omark@gmail.com>
2022-06-24 14:38:06 -0400

    Import axios to Reviews and create AddLocation component

--- src/frontend/pages/Reviews.js
+++ src/frontend/pages/Reviews.js
@@ -1,8 +1,13 @@
 import React from 'react';
+const axios = require('axios');
 
 class Reviews extends React.Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            locations: ""
+        };
     }
 
     render(){
