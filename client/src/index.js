4a138841b6605fd1f0e30304d0ea671c3fe810fa
Omar <niazi.omark@gmail.com>
2022-07-02 17:13:25 -0400

    Adjust file structure for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import App from "./frontend/App.js";
+import App from "./main/App.js";
 import './frontend/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
